b36f74a659446e9e977d426b78e52eab76826f88
Rodrick Sims <rsimstransport@gmail.com>
2021-09-28 19:08:41 -0400

    updated server.js

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ app.use(express.static("public"));
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/budget", {
   useNewUrlParser: true,
   useUnifiedTopology: true,
-  useCreateIndex: true,
+  //useCreateIndex: true,
   useFindAndModify: false,
 });
 
